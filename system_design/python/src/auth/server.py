0b310ab50dd1be9ffa89ed7cef4f65183a999fea
Maryll Castelino <maryllcastelino@gmail.com>
2023-03-10 11:56:10 +0000

    auth flow and jwt

--- system_design/python/src/auth/server.py
+++ system_design/python/src/auth/server.py
@@ -15,7 +15,7 @@ server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
 
 
 # create a route for login with flask
-@server.route('/login', method=["POST"])
+@server.route('/login', methods=["POST"])
 def login():
     auth = request.authorization
     if not auth:
@@ -38,3 +38,39 @@ def login():
             return createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
     else:
         return "invalid credentials", 401
+
+@server.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+
+    if not encoded_jwt:
+        return "missing credentials", 401
+    
+    # jwt is of the format : <type> <token> so we extract the token
+    # we assume that the type is "Bearer" but in a production environment make sure check the type
+    encoded_jwt = encoded_jwt.split(" ")[1]
+
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, os.environ.get("JWT_SECRET"), algorithm=["HS256"]
+        )
+    except:
+        return "not authorized", 403
+
+    return decoded, 200
+
+def createJWT(username, secret, authz):
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1),
+            "iat": datetime.datetime.now(tz=datetime.timezone.utc),
+            "admin": authz,
+        },
+        secret,
+        algorithm="HS256",
+    )
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=5000)
